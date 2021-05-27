8f909d9703063eccf5d8953e24ca50753a23a684
Branko Gvoka <bane92ru@gmail.com>
2021-05-27 02:18:03 +0200

    Remove unused deps

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,1 +1,15 @@
-export * as Button from './components/Button/Button'
+import { colors } from "./theme/colors";
+import { shadows } from "./theme/shadows";
+import { typography } from "./theme/typography";
+
+export { default as Button } from './components/Button/Button'
+export { default as Input } from './components/Input/Input'
+export { default as Select } from './components/Select/Select'
+export { default as Slider } from './components/Slider/Slider'
+export { default as Typography } from './components/Typography/Typography'
+
+export {
+  colors,
+  shadows,
+  typography
+}
